b3c8428fa9c9e756bb087f9bbbbf841000aa7aff
Jillian Lumbres <jillianperezlumbres@gmail.com>
2026-04-07 21:37:20 +0800

    Implement CoordinateCounter class using pure Python

--- coordinate_counter.py
+++ coordinate_counter.py
@@ -1,4 +1,27 @@
 class CoordinateCounter:
-    def __init__(self, width: int, height: int): ...
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
 
-    def update(self, x: int, y: int): ...
+        self.grid = []
+
+        for row_index in range(height):
+            current_row = []
+
+            for column_index in range(width):
+                current_row.append(0)
+
+            self.grid.append(current_row)
+
+    def update(self, x: int, y: int):
+        if x >= 0 and x < self.width:
+            
+            if y >= 0 and y < self.height:
+                
+                self.grid[y][x] = self.grid[y][x] + 1
+            
+            else:
+                print("Y coordinate is outside the grid.")
+
+        else:
+            print("X coordinate is outside the grid.")
